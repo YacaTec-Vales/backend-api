245fef339742e932f2729d2e5b24b3b9981c12ee
XxjesusalbertoxX <villarrealperezjesusalberto@gmail.com>
2026-08-17 18:58:43 -0600

    fix(conciliacion): importar auth y database module en reconciliations module

--- src/reconciliations/reconciliations.module.ts
+++ src/reconciliations/reconciliations.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { DatabaseModule } from '../database/database.module';
 import { ExcelParserService } from './services/excel-parser.service';
 import { ConciliacionService } from './services/conciliacion.service';
 import { ConciliacionesController } from './conciliaciones.controller';
 
 @Module({
+  imports: [AuthModule, DatabaseModule],
   controllers: [ConciliacionesController],
   providers: [ExcelParserService, ConciliacionService],
   exports: [ExcelParserService, ConciliacionService],
